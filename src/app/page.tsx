92ec3c51cba42408e43f11f7b7088eae14a84b62
toledompm <toledompm@gmail.com>
2023-06-13 23:42:56 -0300

    feat: redesign feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-import Image from 'next/image';
 
 type Post = {
   id: number;
@@ -14,16 +12,14 @@ export default function Home() {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   }));
 
-  postsData[0].description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  postsData[0].description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet."
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-24">
-      <div className="bg-gray-800 shadow-md rounded-lg w-full sm:w-70 h-full sm:h-70vh flex flex-col items-center justify-center border-2 border-gray-100">
-        <ul className="grid grid-cols-1">
-          {postsData.map((post) => (
-            <PostCard key={post.id} post={post}/>
-          ))}
-        </ul>
+    <main className="flex min-h-screen flex-col items-center justify-center py-4 sm:py-8 md:py-16 lg:py-20 px-4 sm:px-24 md:px-48 lg:px-72">
+      <div className="bg-gray-800 shadow-md rounded-lg w-full h-full flex flex-col items-center justify-center border-2 border-gray-100">
+        {postsData.map((post) => (
+          <PostCard key={post.id} post={post}/>
+        ))}
       </div>
     </main>
   );
@@ -37,42 +33,31 @@ function Divider() {
 
 function PostCard({post}: {post: Post}) {
   return (
-    <div className="sm:col-span-1 lg:col-span-1 sm:row-span-1 lg:row-span-1">
-        <div className="block w-full h-full">
-          <div className="grid grid-cols-12">
-            <div className="p-4 col-span-1">
-              <Image
-                src="https://via.placeholder.com/150"
-                alt="Picture of the author"
-                width="150"
-                height="150"
-                className="rounded-full"
-              />
-            </div>
-            <div className="sm:col-span-11 py-4 pr-8 block">
-              <div className="">
-                <h3 className="text-xl font-semibold mb-2 text-white">{post.title}</h3>
-              </div>
-              <div className="">
-                <p className="text-white">{post.description}</p>
-              </div>
-              <div className="bg-gray-700 rounded-lg p-10 my-5 w-auto">
-              </div>
-              <div className="flex">
-                {['tag1', 'tag2', 'tag3'].map((tag) => (TagBubble({tag})))}
-              </div>
-            </div>
+    <div className="block w-full h-full">
+      <div className="py-4 px-8">
+        <div className="text-xl font-semibold text-white mb-2">
+          <h3>{post.title}</h3>
+        </div>
+        <div className="text-white mb-4">
+          <p>{post.description}</p>
+        </div>
+        <div className="bg-gray-700 rounded-lg mb-2 pb-40 sm:pb-80 md:pb-120 lg:pb-160">
+          <div className="bg-white rounded-t-lg pb-6">
           </div>
         </div>
-        {Divider()}
+        <div className="flex">
+          {['tag1', 'tag2', 'tag3'].map((tag) => (TagBubble({tag})))}
+        </div>
+      </div>
+      {Divider()}
     </div>
   );
 }
 
 function TagBubble({tag}: {tag: string}) {
   return (
-    <div className="bg-green-700 rounded-full px-2 py-1 text-white mr-2 my-2 text-sm">
-      {tag}
+    <div className="text-gray-600 mr-2 text-sm my-2">
+      #{tag}
     </div>
   );
 }
